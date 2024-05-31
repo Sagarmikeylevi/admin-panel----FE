0c6dffd812ebf42a27d96979b33fa48c0cd299dc
Sagar das <sagarmikeylevi@gmail.com>
2024-05-31 10:34:03 +0530

    add: add new employee section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeListComponent } from './core/employe-list/employe-list.component';
+import { AddNewEmployeeComponent } from './core/add-new-employee/add-new-employee.component';
 
-const routes: Routes = [{ path: '', component: EmployeListComponent }];
+const routes: Routes = [
+  { path: '', component: EmployeListComponent },
+  { path: 'addForm', component: AddNewEmployeeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,22 @@ import { SidebarComponent } from './core/sidebar/sidebar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { EmployeListComponent } from './core/employe-list/employe-list.component';
+import { AddNewEmployeeComponent } from './core/add-new-employee/add-new-employee.component';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [AppComponent, SidebarComponent, EmployeListComponent],
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    EmployeListComponent,
+    AddNewEmployeeComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatTableModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
